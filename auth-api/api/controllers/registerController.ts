9580fede13f0acdefd6e2931bc1d2625da421a4d
Dawid Dahl <dawiddahl@gmail.com>
2020-05-07 17:00:59 +0200

    refactored to auth reg-controller to async/await

--- auth-api/api/controllers/registerController.ts
+++ auth-api/api/controllers/registerController.ts
@@ -6,7 +6,7 @@ import bcrypt from "bcrypt";
 import {authJsonResponse, generateId} from "../utils/utils";
 import fetch from "node-fetch";
 
-export const registerController = (req: Request, res: Response) => {
+export const registerController = async (req: Request, res: Response) => {
 	const errors = validationResult(req);
 
 	if (errors.isEmpty()) {
@@ -16,72 +16,67 @@ export const registerController = (req: Request, res: Response) => {
 			err ? console.error(err) : console.log("Connected to the SQLite database")
 		);
 
-		bcrypt.hash(req.body.password, 10, (err, hash) => {
+		bcrypt.hash(req.body.password, 10, async (err, hash) => {
 			if (err) {
 				console.error(err);
 				return;
 			}
 
-			fetch(`${process.env.MAIN_FETCH_URL}/api/ping`)
-				.then(() => {
-					const id = generateId();
-					const sql = `INSERT INTO ${Tables.auth_users} (id, email, password) VALUES (?, ?, ?)`;
-					const values = [id, req.body.email, hash];
+			try {
+				await fetch(`${process.env.MAIN_FETCH_URL}/api/ping`);
 
-					db.run(sql, values, async err => {
-						if (err) {
-							console.log(err);
+				const id = generateId();
+				const sql = `INSERT INTO ${Tables.auth_users} (id, email, password) VALUES (?, ?, ?)`;
+				const values = [id, req.body.email, hash];
 
-							const errorMsg = err.message.includes(
-								"UNIQUE constraint failed: AuthUsers.email"
-							)
-								? "A user with that email is already registered."
-								: "Couldn't register user.";
+				db.run(sql, values, async err => {
+					if (err) {
+						console.log(err);
 
-							res.status(403).json(authJsonResponse(false, {message: errorMsg}));
+						const errorMsg = err.message.includes(
+							"UNIQUE constraint failed: AuthUsers.email"
+						)
+							? "A user with that email is already registered."
+							: "Couldn't register user.";
 
-							db.close(err =>
-								err
-									? console.error(err)
-									: console.log("Closed the database connection")
+						res.status(403).json(authJsonResponse(false, {message: errorMsg}));
+
+						db.close(err =>
+							err ? console.error(err) : console.log("Closed the database connection")
+						);
+					} else {
+						try {
+							await fetch(`${process.env.MAIN_FETCH_URL}/api/users/create`, {
+								method: "POST",
+								headers: {
+									"Content-Type": "application/json",
+								},
+								body: JSON.stringify({id, username: req.body.username}),
+							});
+
+							res.status(200).json(
+								authJsonResponse(true, {
+									message:
+										"Registration successful. Welcome! You can now log in!",
+								})
 							);
-						} else {
-							try {
-								await fetch(`${process.env.MAIN_FETCH_URL}/api/users/create`, {
-									method: "POST",
-									headers: {
-										"Content-Type": "application/json",
-									},
-									body: JSON.stringify({id, username: req.body.username}),
-								});
-
-								res.status(200).json(
-									authJsonResponse(true, {
-										message:
-											"Registration successful. Welcome! You can now log in!",
-									})
-								);
-							} catch (e) {
-								console.error(e);
-							}
-
-							db.close(err =>
-								err
-									? console.error(err)
-									: console.log("Closed the database connection")
-							);
+						} catch (e) {
+							console.error(e);
 						}
-					});
-				})
-				.catch(e => {
-					console.error("Could not connect to main API. No user created --", e);
-
-					res.status(200).json(
-						authJsonResponse(false, {
-							message: "Registration is not possible right now. Sorry!",
-						})
-					);
+						db.close(err =>
+							err ? console.error(err) : console.log("Closed the database connection")
+						);
+					}
 				});
+			} catch (e) {
+				console.error("Could not connect to main API. No user created --", e);
+
+				res.status(200).json(
+					authJsonResponse(false, {
+						message: "Registration is not possible right now. Sorry!",
+					})
+				);
+			}
 		});
 	} else {
 		res.status(422).send("Invalid Registration, try again!");
